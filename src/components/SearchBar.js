3f7a2b76f9ef07a9af1044546832888741d1ce67
Edmond Atto <edmond091@gmail.com>
2018-01-30 20:21:28 +0300

    Add form validation to the search field

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,8 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
     this.state = {
-      searchString: ''
+      searchString: '',
+      invalidInput: false
     }
   }
 
@@ -16,16 +17,25 @@ class SearchBar extends Component {
   handleSearch = event => {
     if (this.state.searchString.trim() === ''){
       event.preventDefault();
+      this.setState({
+        invalidInput: true
+      });
       return null;
     }else {
       this.props.onSearch(this.state.searchString);
       this.props.toggleLoading();
+      this.setState({
+        invalidInput: false
+      });
     }
   };
 
   render(){
+    const invalidInput = this.state.invalidInput;
+    const formClass = invalidInput ? 'form-inline has-danger mt-2 mb-4' : 'form-inline mt-2 mb-4';
+
     return(
-      <form className="form-inline mt-2 mb-4" id="search-bar" onSubmit={this.handleSearch}>
+      <form className={formClass} id="search-bar" onSubmit={this.handleSearch}>
         <input
           type="text"
           className="form-control form-control-lg col-md-10 mb-2 mr-sm-2 mb-sm-0"
@@ -39,6 +49,7 @@ class SearchBar extends Component {
           className="btn btn-lg btn-primary">
           SEARCH
         </button>
+        {invalidInput && <div className="form-control-feedback">Invalid search term!</div>}
       </form>
     );
   }
